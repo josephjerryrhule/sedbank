9f860dbbb89ee369736639a9b360aa0216aaa940
Joseph <josephjerryrhule@gmail.com>
2023-01-03 19:21:10 +0000

    first commit

---
+++ src/components/Billing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
+
+const Billing = () => {
+  return <div>Billing</div>;
+};
+
+export default Billing;
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Business from "./Business";
+import Billing from "./billing";
+import CardDeal from "./CardDeal";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
