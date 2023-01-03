7c430a0adc78659670fefd9aacad550441a5fc39
Joseph <josephjerryrhule@gmail.com>
2023-01-03 19:37:03 +0000

    completed card deal section. Fixed Billing section display error

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Stats from "./Stats";
 import Business from "./Business";
-import Billing from "./billing";
+import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Testimonials from "./Testimonials";
 import Clients from "./Clients";
